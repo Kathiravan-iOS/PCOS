2a65750de3002e1b96c516c47b50ce76f2684c43
kathiravan <kathiravan.sse@saveetha.com>
2024-05-07 12:03:11 +0530

    weightgraph

--- Pcos/ViewController/Doctor/TableViewCell/WeightMeasureCell.swift
+++ Pcos/ViewController/Doctor/TableViewCell/WeightMeasureCell.swift
@@ -1,119 +1,86 @@
+//
+//  WeightMeasureCell.swift
+//  Pcos
+//
+//  Created by Karthik Babu on 16/10/23.
+//
+
 import UIKit
 
 class WeightMeasureCell: UITableViewCell {
+    var name : String = ""
     @IBOutlet weak var chartView: UIView!
-    var name: String = ""
     var lineChartView: LineChartView!
-
     override func awakeFromNib() {
         super.awakeFromNib()
-        setupLineChartView()
-        fetchData()
-    }
-
-    private func setupLineChartView() {
         lineChartView = LineChartView()
-        chartView.addSubview(lineChartView)
-        lineChartView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            lineChartView.topAnchor.constraint(equalTo: chartView.topAnchor),
-            lineChartView.trailingAnchor.constraint(equalTo: chartView.trailingAnchor),
-            lineChartView.leadingAnchor.constraint(equalTo: chartView.leadingAnchor),
-            lineChartView.bottomAnchor.constraint(equalTo: chartView.bottomAnchor)
-        ])
+           chartView.addSubview(lineChartView)
+           // Set Auto Layout constraints for lineChartView
+           lineChartView.translatesAutoresizingMaskIntoConstraints = false
+           NSLayoutConstraint.activate([
+               lineChartView.topAnchor.constraint(equalTo: chartView.topAnchor),
+               lineChartView.trailingAnchor.constraint(equalTo: chartView.trailingAnchor),
+               lineChartView.leadingAnchor.constraint(equalTo: chartView.leadingAnchor),
+               lineChartView.bottomAnchor.constraint(equalTo: chartView.bottomAnchor)
+           ])
     }
-
-    private func fetchData() {
-        guard let url = URL(string: "\(ServiceAPI.baseURL)weight_graph.php") else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-
-        let parameters = "name=\(name)"
-        request.httpBody = parameters.data(using: .utf8)
-
-        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            guard let data = data, error == nil else {
-                print("Network error:", error ?? "Unknown error")
-                return
-            }
-
-            do {
-                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200,
-                   let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                   let dataArray = json["data"] as? [[String: Any]] {
-                    let weightValues = dataArray.map { dataPoint -> CGFloat in
-                        if let weightString = dataPoint["weight_value"] as? String, let weight = Double(weightString) {
-                            return CGFloat(weight)
-                        }
-                        return 0.0
-                    }
-                    DispatchQueue.main.async {
-                        self?.lineChartView.dataPoints = weightValues
-                        self?.lineChartView.setNeedsDisplay()
-                    }
-                } else {
-                    print("Error: HTTP Status Code: \((response as? HTTPURLResponse)?.statusCode ?? 0) or JSON data is incorrect")
-                }
-            } catch {
-                print("Failed to decode JSON:", error)
-            }
-        }.resume()
-    }
-
-
-
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+
+ 
     }
+    
 }
-
 class LineChartView: UIView {
-    var dataPoints: [CGFloat] = []
-    let circleFillColor = UIColor(hex: "FFC0CB")  // Light Pink
-    let circleRadius: CGFloat = 5.0
-    let lineColor = UIColor(hex: "FF69B4")       // Brighter Pink
+    var dataPoints: [CGFloat] = [10, 15, 12, 18, 20] // Replace with your data points
 
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        backgroundColor = UIColor.white
-    }
+    // Customizable properties
+    let circleFillColor = UIColor(hex: "A0DBFD")
+    let circleRadius: CGFloat = 5.0
+    let lineColor = UIColor(hex: "7876F5")
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor.white
+        self.backgroundColor = UIColor.white // Set the background color to white
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.backgroundColor = UIColor.white // Set the background color to white
     }
 
     override func draw(_ rect: CGRect) {
         super.draw(rect)
-        guard let context = UIGraphicsGetCurrentContext(), !dataPoints.isEmpty else {
+
+        guard let context = UIGraphicsGetCurrentContext() else {
             return
         }
 
-        context.setStrokeColor(lineColor.cgColor)
+        context.setStrokeColor(lineColor.cgColor) // Set line color
         context.setLineWidth(2.0)
 
-        let maxValue = dataPoints.max() ?? 1
         let chartHeight = rect.size.height
         let chartWidth = rect.size.width
-        let pointSpacing = chartWidth / CGFloat(max(dataPoints.count - 1, 1))
+        let pointSpacing = chartWidth / CGFloat(dataPoints.count - 1)
 
         var previousPoint: CGPoint?
 
         for (index, value) in dataPoints.enumerated() {
             let x = pointSpacing * CGFloat(index)
-            let y = chartHeight - (value / maxValue * chartHeight)
+            let y = chartHeight - (value / 20.0) * chartHeight // Scale your data accordingly
 
-            if let prev = previousPoint {
-                context.move(to: prev)
+            if let previousPoint = previousPoint {
+                // Draw a line connecting the current and previous points
+                context.move(to: previousPoint)
                 context.addLine(to: CGPoint(x: x, y: y))
                 context.strokePath()
-            }
 
-            let circleRect = CGRect(x: x - circleRadius, y: y - circleRadius, width: circleRadius * 2, height: circleRadius * 2)
-            context.setFillColor(circleFillColor.cgColor)
-            context.fillEllipse(in: circleRect)
+                // Draw a filled circle at the current point
+                let circleRect = CGRect(x: x - circleRadius, y: y - circleRadius, width: circleRadius * 2, height: circleRadius * 2)
+                context.setFillColor(circleFillColor.cgColor)
+                context.fillEllipse(in: circleRect)
+            }
 
             previousPoint = CGPoint(x: x, y: y)
         }
@@ -122,13 +89,18 @@ class LineChartView: UIView {
 
 extension UIColor {
     convenience init(hex: String) {
-        let hexSanitized = hex.replacingOccurrences(of: "#", with: "").trimmingCharacters(in: .whitespacesAndNewlines)
-        var rgb: UInt32 = 0
-        Scanner(string: hexSanitized).scanHexInt32(&rgb)
+        var hexSanitized = hex
+        hexSanitized = hexSanitized.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
 
         let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
         let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
         let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
         self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
